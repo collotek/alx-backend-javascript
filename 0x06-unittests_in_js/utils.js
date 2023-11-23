289f91c8e00ea25111c0e9be3776aab16ba4846b
collotek <ebiizcomp@gmail.com>
2023-11-23 22:46:13 +0300

    spies

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,25 @@
+class Utils {
+    static calculateNumber(type, a, b) {
+	const ax = Math.round(a);
+	const bx = Math.round(b);
+	let c = 0;
+	switch (type) {
+	case 'SUM':
+            c = ax + bx;
+            break;
+	case 'SUBTRACT':
+            c = ax - bx;
+            break;
+	case 'DIVIDE':
+	    if (bx === 0) {
+		c = "Error";
+	    } else {
+		c = ax / bx;
+	    }
+	    break;
+	}
+	return c;
+    }
+}
+
+module.exports = Utils;
